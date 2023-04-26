3a4804d8dba3f43c0ce1b6a4dbab84c9338d9fdf
jtnunley <jtnunley01@gmail.com>
2023-04-26 10:56:14 -0700

    Increase lock coverage

--- src/lock/mod.rs
+++ src/lock/mod.rs
@@ -112,6 +112,29 @@ mod tests {
     }
 
     #[test]
+    fn test_mutex_get_mut_into_inner() {
+        let mut mutex = Mutex::<i32>::default();
+        *mutex.get_mut() = 3;
+        assert_eq!(mutex.into_inner(), 3);
+    }
+
+    #[test]
+    fn test_mutex_lock_await() {
+        future::block_on(async {
+            let mutex = Mutex::new(0);
+            let mut guard = mutex.lock().await;
+            *guard += 1;
+
+            let lock2 = mutex.lock();
+            futures_lite::pin!(lock2);
+
+            assert!(future::poll_once(&mut lock2).await.is_none());
+            drop(guard);
+            assert!(future::poll_once(&mut lock2).await.is_some());
+        });
+    }
+
+    #[test]
     fn test_rwlock() {
         future::block_on(async {
             let rwlock = RwLock::new(0);
@@ -160,6 +183,39 @@ mod tests {
     }
 
     #[test]
+    fn test_rwlock_get_mut() {
+        let mut rwlock = RwLock::<i32>::default();
+        *rwlock.get_mut() = 3;
+        assert_eq!(rwlock.into_inner(), 3);
+    }
+
+    #[test]
+    fn test_rwlock_read_write_await() {
+        future::block_on(async {
+            let rwlock = RwLock::new(0);
+            let guard = rwlock.write().await;
+            assert_eq!(*guard, 0);
+
+            let read2 = rwlock.read();
+            futures_lite::pin!(read2);
+
+            assert!(future::poll_once(&mut read2).await.is_none());
+            drop(guard);
+            assert!(future::poll_once(&mut read2).await.is_some());
+
+            let guard = rwlock.read().await;
+            assert_eq!(*guard, 0);
+
+            let write2 = rwlock.write();
+            futures_lite::pin!(write2);
+
+            assert!(future::poll_once(&mut write2).await.is_none());
+            drop(guard);
+            assert!(future::poll_once(&mut write2).await.is_some());
+        });
+    }
+
+    #[test]
     fn test_semaphore() {
         future::block_on(async {
             let semaphore = Semaphore::new(1);
@@ -168,6 +224,93 @@ mod tests {
             // If we try to acquire the semaphore again, it will block.
             assert!(semaphore.try_acquire().is_none());
             assert!(future::poll_once(semaphore.acquire()).await.is_none());
+        });
+    }
+
+    #[test]
+    fn test_semaphore_await() {
+        future::block_on(async {
+            let semaphore = Semaphore::new(1);
+            let _guard = semaphore.acquire().await;
+
+            // If we try to acquire the semaphore again, it will block.
+            assert!(semaphore.try_acquire().is_none());
+
+            let acquire = semaphore.acquire();
+            futures_lite::pin!(acquire);
+
+            assert!(future::poll_once(&mut acquire).await.is_none());
+            drop(_guard);
+            assert!(future::poll_once(&mut acquire).await.is_some());
+        });
+    }
+
+    #[cfg(feature = "alloc")]
+    #[test]
+    fn test_semaphore_rc() {
+        use alloc::rc::Rc;
+
+        future::block_on(async {
+            let semaphore = Rc::new(Semaphore::new(1));
+            let _guard = semaphore.clone().acquire_rc().await;
+
+            // If we try to acquire the semaphore again, it will block.
+            assert!(semaphore.clone().try_acquire_rc().is_none());
+            assert!(future::poll_once(semaphore.acquire()).await.is_none());
+        });
+    }
+
+    #[cfg(feature = "alloc")]
+    #[test]
+    fn test_semaphore_rc_await() {
+        use alloc::rc::Rc;
+
+        future::block_on(async {
+            let semaphore = Rc::new(Semaphore::new(1));
+            let _guard = semaphore.clone().acquire_rc().await;
+
+            // If we try to acquire the semaphore again, it will block.
+            assert!(semaphore.clone().try_acquire_rc().is_none());
+
+            let acquire = semaphore.acquire_rc();
+            futures_lite::pin!(acquire);
+
+            assert!(future::poll_once(&mut acquire).await.is_none());
+            drop(_guard);
+            assert!(future::poll_once(&mut acquire).await.is_some());
+        });
+    }
+
+    #[test]
+    fn once_cell_smoke() {
+        future::block_on(async {
+            let cell = OnceCell::<i32>::new();
+            assert!(cell.get().is_none());
+
+            let value = cell.get_or_init(async { 5 }).await;
+            assert_eq!(*value, 5);
+            assert_eq!(cell.get().unwrap(), &5);
+        });
+    }
+
+    #[test]
+    fn once_cell_get_mut() {
+        let mut cell = OnceCell::<i32>::from(5);
+        assert!(cell.get_mut().is_some());
+        assert_eq!(cell.take(), Some(5));
+    }
+
+    #[test]
+    fn once_cell_wait() {
+        future::block_on(async {
+            let cell = OnceCell::<i32>::new();
+
+            let waiter = cell.wait();
+            futures_lite::pin!(waiter);
+
+            assert!(future::poll_once(&mut waiter).await.is_none());
+            cell.set(5).await.unwrap();
+            assert!(future::poll_once(&mut waiter).await.is_some());
         });
     }
 }
