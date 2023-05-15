2aca56a656ba065174ce034cb5d77c15ad210add
jtnunley <jtnunley01@gmail.com>
2023-05-15 16:08:28 -0700

    Initial setup for comparisons

---
+++ comparisons/run_comparison.py
@@ -0,0 +1,96 @@
+# SPDX-License-Identifier: LGPL-3.0-or-later OR MPL-2.0
+# This file is a part of `unsend`.
+#
+# `unsend` is free software: you can redistribute it and/or modify it under the
+# terms of either:
+#
+# * GNU Lesser General Public License as published by the Free Software Foundation, either
+#   version 3 of the License, or (at your option) any later version.
+# * Mozilla Public License as published by the Mozilla Foundation, version 2.
+# * The Patron License (https://github.com/notgull/unsend/blob/main/LICENSE-PATRON.md)
+#   for sponsors and contributors, who can ignore the copyleft provisions of the above licenses
+#   for this project.
+#
+# `unsend` is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+# PURPOSE. See the GNU Lesser General Public License or the Mozilla Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License and the Mozilla
+# Public License along with `unsend`. If not, see <https://www.gnu.org/licenses/>.
+
+# Run comparisons between different async runtimes running a basic HTTP server.
+
+import subprocess as sp
+import os
+import sys
+import matplotlib.pyplot as plt
+
+def processWrkOutput(output):
+    """Processes the output of the `wrk` command"""
+
+    # Split output by newlines.
+    lines = output.split("\n")
+
+    # Get the line with "Req/Sec" in it
+    reqSecLine = [line for line in lines if "Req/Sec" in line][0]
+
+    # Split by whitespace and filter out empty strings.
+    reqSecLine = [item for item in reqSecLine.split(" ") if item != ""]
+
+    # Get the average number of requests per second
+    reqSec = reqSecLine[1]
+
+    # If it ends with "k", multiply by 1000.
+    if reqSec.endswith("k"):
+        reqSec = float(reqSec[:-1]) * 1000
+    else:
+        reqSec = float(reqSec)
+
+    # Return the average number of requests per second.
+    return reqSec
+
+def runWrkForPackage(package):
+    """Runs the `wrk` command for the given package"""
+
+    # Run the "cargo build --release" command for the package.
+    sp.run(["cargo", "build", "--release", "-p", package])
+
+    # In the background, use cargo run to run the package.
+    process = sp.Popen(["cargo", "run", "--release", "-p", package], stdout=sp.PIPE, stderr=sp.PIPE)
+
+    # Run the wrk command.
+    wrk = sp.run(["wrk", "-t12", "-c400", "-d30s", "http://localhost:8000/index.html"], stdout=sp.PIPE, stderr=sp.PIPE)
+
+    # Kill the process.
+    process.kill()
+
+    # Return the output.
+    output = wrk.stdout.decode("utf-8")
+    return processWrkOutput(output)
+
+def runCollectionAndPlot(packages, outPath):
+    """Runs the list of packages and plots them"""
+
+    # Run the wrk command for each package.
+    results = [runWrkForPackage(package) for package in packages]
+
+    # Plot the results.
+    plt.bar(packages, results)
+    plt.ylabel("Requests per second")
+    plt.savefig(outPath)
+
+HELLO = ["unsend-hello", "tokio-hello"]
+
+def main():
+    """Main function"""
+
+    # Create the output directory.
+    os.makedirs("out", exist_ok=True)
+
+    # Run the hello world example.
+    runCollectionAndPlot(HELLO, "out/hello.png")
+
+if __name__ == "__main__":
+    main()
+
---
+++ comparisons/tokio-hello/src/main.rs
@@ -0,0 +1,117 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later OR MPL-2.0
+// This file is a part of `unsend`.
+//
+// `unsend` is free software: you can redistribute it and/or modify it under the
+// terms of either:
+//
+// * GNU Lesser General Public License as published by the Free Software Foundation, either
+//   version 3 of the License, or (at your option) any later version.
+// * Mozilla Public License as published by the Mozilla Foundation, version 2.
+// * The Patron License (https://github.com/notgull/unsend/blob/main/LICENSE-PATRON.md)
+//   for sponsors and contributors, who can ignore the copyleft provisions of the above licenses
+//   for this project.
+//
+// `unsend` is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+// PURPOSE. See the GNU Lesser General Public License or the Mozilla Public License for more
+// details.
+//
+// You should have received a copy of the GNU Lesser General Public License and the Mozilla
+// Public License along with `unsend`. If not, see <https://www.gnu.org/licenses/>.
+
+use futures_lite::io::BufReader;
+use futures_lite::prelude::*;
+
+use std::net::TcpListener;
+
+fn main() {
+    let rt = tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+    if let Err(e) = rt.block_on(main2()) {
+        eprintln!("Error: {}", e);
+        std::process::exit(1);
+    }
+}
+
+async fn main2() -> Result<(), Box<dyn std::error::Error>> {
+    // Bind to a port.
+    let listener = async_io::Async::<TcpListener>::bind(([127, 0, 0, 1], 8000))?;
+
+    // Wait for incoming connections.
+    println!("Listening on {:?}", listener.get_ref().local_addr()?);
+    let incoming = listener.incoming();
+    futures_lite::pin!(incoming);
+    incoming
+        .try_for_each(|conn| {
+            conn.map(|conn| {
+                // Spawn a new task.
+                tokio::spawn(async move {
+                    if let Err(e) = handle_connection(conn).await {
+                        eprintln!("Error: {}", e);
+                    }
+                });
+            })
+        })
+        .await?;
+
+    Ok(())
+}
+
+async fn handle_connection(
+    mut conn: impl AsyncRead + AsyncWrite + Unpin,
+) -> Result<(), Box<dyn std::error::Error>> {
+    // Read HTTP headers until we get to "\r\n\r\n"
+    let mut data = Vec::new();
+    let mut buf_reader = BufReader::new(&mut conn);
+    loop {
+        let len = buf_reader.read_until(b'\n', &mut data).await?;
+        if len == 0 {
+            // EOF
+            break;
+        }
+
+        // If we got the end of the headers, stop.
+        if data.ends_with(b"\r\n\r\n") {
+            break;
+        }
+    }
+
+    // Parse the result to get the body using httparse.
+    let mut headers = [httparse::EMPTY_HEADER; 16];
+    let mut req = httparse::Request::new(&mut headers);
+
+    // Find the Content-Length header.
+    req.parse(&data)?;
+    let body_length = req
+        .headers
+        .iter()
+        .find(|h| h.name.eq_ignore_ascii_case("Content-Length"))
+        .map(|h| std::str::from_utf8(h.value).unwrap().parse::<usize>())
+        .transpose()?
+        .unwrap_or(0);
+
+    if body_length == 0 {
+        // Just send a hello world.
+        conn.write_all(b"HTTP/1.1 200 OK\r\nContent-Length: 12\r\n\r\nHello world!")
+            .await?;
+        return Ok(());
+    }
+
+    // Read the body.
+    let mut body = Vec::new();
+    buf_reader
+        .take(body_length as u64)
+        .read_to_end(&mut body)
+        .await?;
+
+    // Write an HTTP response containing the body.
+    conn.write_all(b"HTTP/1.1 200 OK\r\nContent-Length: ")
+        .await?;
+    conn.write_all(body.len().to_string().as_bytes()).await?;
+    conn.write_all(b"\r\n\r\n").await?;
+    conn.write_all(&body).await?;
+
+    Ok(())
+}
